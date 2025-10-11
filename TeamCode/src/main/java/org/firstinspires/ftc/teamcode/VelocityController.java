3d407f3411475b66f1ec1db7ad918a6534b68c8b
Rowan Shenoy <rowans@hotmail.com>
2025-10-11 12:33:16 -0400

    save

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityController.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class VelocityController extends LinearOpMode {
+    DcMotorEx leftMotor;
+    DcMotorEx rightMotor;
+
+    int RPM_Target = 60;
+
+    public void runOpMode(){
+        leftMotor = hardwareMap.get(DcMotorEx.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotorEx.class, "rightMotor");
+
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        leftMotor.setVelocity(RPM_Target/60*28);
+        rightMotor.setVelocity(RPM_Target/60*28);
+    }
+}
