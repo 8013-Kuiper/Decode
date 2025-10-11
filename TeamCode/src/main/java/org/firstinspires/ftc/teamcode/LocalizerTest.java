6a8376f447c35c433217e64f5f3614f54ce26a6e
Rowan Shenoy <rowans@hotmail.com>
2025-10-11 12:11:55 -0400

    Creates class to test odometry wheels
    Will be used to figure out direction of wheels as well as equations

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LocalizerTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class LocalizerTest extends LinearOpMode {
+    DcMotor leftPod;
+    DcMotor rightPod;
+    DcMotor lateralPod;
+
+    public void runOpMode() {
+        leftPod = hardwareMap.get(DcMotor.class, "frontRight");
+        rightPod = hardwareMap.get(DcMotor.class, "frontLeft");
+        lateralPod = hardwareMap.get(DcMotor.class, "backLeft");
+
+        leftPod.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightPod.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lateralPod.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftPod.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightPod.setDirection(DcMotorSimple.Direction.FORWARD);
+        lateralPod.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            telemetry.addData("Left Pod", leftPod.getCurrentPosition());
+            telemetry.addData("Right Pod", rightPod.getCurrentPosition());
+            telemetry.addData("Lateral Pod", lateralPod.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+
+}
