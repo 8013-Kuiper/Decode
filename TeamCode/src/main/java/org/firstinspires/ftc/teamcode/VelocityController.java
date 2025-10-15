d932bfafbe2091167c790784d9f4ca64f62ce8ef
Rowan Shenoy <rowans@hotmail.com>
2025-10-14 21:32:28 -0400

    Creates velocity controller to test flywheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityController.java
@@ -3,26 +3,44 @@ package org.firstinspires.ftc.teamcode;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@TeleOp
 public class VelocityController extends LinearOpMode {
     DcMotorEx leftMotor;
     DcMotorEx rightMotor;
 
-    int RPM_Target = 60;
+    gamepadEx gamepad1
+
+    double RPM_Target = 5000;
 
     public void runOpMode(){
         leftMotor = hardwareMap.get(DcMotorEx.class, "leftMotor");
         rightMotor = hardwareMap.get(DcMotorEx.class, "rightMotor");
+
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
-        leftMotor.setVelocity(RPM_Target/60*28);
-        rightMotor.setVelocity(RPM_Target/60*28);
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                RPM_Target += 50;
+            } else if (gamepad1.b) {
+                RPM_Target -= 50;
+            } else if (gamepad1.x) {
+                RPM_Target = 0;
+            }
+            leftMotor.setVelocity(RPM_Target / 60 * 28);
+            rightMotor.setVelocity(RPM_Target / 60 * 28);
+
+            telemetry.addData("RPM", RPM_Target);
+            telemetry.update();
+        }
     }
 }
