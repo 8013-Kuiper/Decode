4e7ce231b8db3fb17e8c85a4953022df5eedcbed
KingTryHard1 <codeshb7@gmail.com>
2025-10-14 21:29:26 -0400

    Fly Wheel stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 public abstract class DriveConstants extends LinearOpMode {
+
+    public DcMotorEx flyWheel;
     public void initRobot(){
+        flyWheel = hardwareMap.get(DcMotorEx.class, "flyWheel");
 
     }
 }
