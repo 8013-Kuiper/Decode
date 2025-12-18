5be260cd7b2b660eb9c1a79c074f473be51f4628
Rowan Shenoy <rowans@hotmail.com>
2025-12-18 17:28:28 -0500

    Functioning 1 artifact auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,34 +1,71 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous
 public class Auto extends LinearOpMode {
-    DcMotor frontLeft, frontRight, backLeft, backRight;
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+
+    private int pathState;
+
+    Servo elevator;
+    DcMotorEx launcher;
+
+    private final Pose startPose = new Pose(57, 135, Math.toRadians(270));
+    private final Pose scorePose = new Pose(60,84, Math.toRadians(135));
 
     public void runOpMode() {
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        elevator = hardwareMap.get(Servo.class, "gate");
+        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        elevator.setPosition(0.5);
+        launcher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        elevator.setPosition(0.5);
 
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startPose);
+
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+
+        Path shootPreload = new Path(new BezierLine(startPose, scorePose));
+        shootPreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
+
+        Path henryIsALoser = new Path(new BezierLine(scorePose, new Pose(50,50,Math.toRadians(135))));
+        henryIsALoser.setConstantHeadingInterpolation(Math.toRadians(135));
 
         waitForStart();
 
-        frontLeft.setPower(0.5);
-        frontRight.setPower(0.5);
-        backLeft.setPower(0.5);
-        backRight.setPower(0.5);
+        follower.followPath(shootPreload);
 
-        sleep (500);
+        while (follower.isBusy()){
+            sleep(50);
+            follower.update();
+        }
 
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        backLeft.setPower(0);
-        backRight.setPower(0);
+        launcher.setPower(1);
+        elevator.setPosition(0);
+        sleep(2500);
+        elevator.setPosition(1);
+        sleep(500);
+        elevator.setPosition(0.5);
+
+        follower.followPath(henryIsALoser);
+        while (follower.isBusy()){
+            sleep(50);
+            follower.update();
+        }
     }
 }
