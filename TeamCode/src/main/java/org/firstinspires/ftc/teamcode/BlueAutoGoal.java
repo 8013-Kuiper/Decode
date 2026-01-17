fa2af07612ffcb063c8e464a67a1fd645aa10934
Rowan Shenoy <rowans@hotmail.com>
2026-01-17 07:03:16 -0500

    Update BlueAutoGoal.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAutoGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAutoGoal.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
@@ -11,24 +12,35 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
 
 @Autonomous (name = "Blue Side Goal")
 public class BlueAutoGoal extends LinearOpMode {
+    AprilTagProcessor aprilTag;
+    VisionPortal visionPortal;
+
     private Follower follower;
     private Timer opmodeTimer;
     DcMotor turretRotatation;
 
     Servo gate;
-    DcMotorEx launcher;
+    MotorEx launcher;
     Servo spindex;
+
+    int targetRPM;
 
     private final Pose startPose = new Pose(27, 132, Math.toRadians(-36));
     private final Pose scorePose = new Pose(60,100, Math.toRadians(125));
 
     public void runOpMode() {
         gate = hardwareMap.get(Servo.class, "gate");
-        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        launcher = new MotorEx(hardwareMap, "launcher", 28, 6000);
         turretRotatation = hardwareMap.get(DcMotor.class, "turret");
         spindex = hardwareMap.get(Servo.class, "rotate");
 
@@ -37,8 +49,9 @@ public class BlueAutoGoal extends LinearOpMode {
         spindex.setPosition(0.5);
         gate.setPosition(1);
 
-        launcher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launcher.setRunMode(MotorEx.RunMode.VelocityControl);
+        launcher.setVeloCoefficients(20, 0, 0);
+        launcher.setFeedforwardCoefficients(0.35, 0.35);
 
         follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(startPose);
@@ -52,26 +65,65 @@ public class BlueAutoGoal extends LinearOpMode {
         Path alignPickup1 = new Path(new BezierLine(scorePose, new Pose(42, 84, Math.toRadians(180))));
         alignPickup1.setConstantHeadingInterpolation(Math.toRadians(135));
 
+        initAprilTag();
+
         waitForStart();
 
-        follower.followPath(shootPreload);
-
-        while (follower.isBusy()){
-            sleep(50);
-            follower.update();
-        }
-
-//        launcher.setPower(1);
-//        gate.setPosition(0);
-        sleep(2500);
-//        gate.setPosition(1);
-        sleep(500);
-//        gate.setPosition(0.5);
-
-//        follower.followPath(alignPickup1);
+//        follower.followPath(shootPreload);
+//
 //        while (follower.isBusy()){
 //            sleep(50);
 //            follower.update();
 //        }
+
+//        double distance = -1;
+//        while (distance == -1) {
+//            distance = getDistance(false);
+//            if (distance < 50) {
+//                targetRPM = (int) (16 * distance + 3205);
+//            } else {
+//                targetRPM = (int) (16 * distance + 3155);
+//            }
+//            telemetry.addData("distance", distance);
+//            telemetry.addData("target RPM", targetRPM);
+//            telemetry.update();
+//        }
+
+//        launcher.setVelocity((((double) targetRPM /60*28)-750));
+        sleep (1250);
+        gate.setPosition(0);
+        sleep (150);
+        gate.setPosition(1);
+        sleep(500);
+        spindex.setPosition(1);
+        sleep(900);
+    }
+
+    private void initAprilTag() {
+        // Create the AprilTag processor the easy way.
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+
+        // Create the vision portal the easy way.
+        visionPortal = VisionPortal.easyCreateWithDefaults(
+                hardwareMap.get(WebcamName.class, "Webcam 1"), aprilTag);
+
+    }
+
+    private double getDistance(boolean isRed) {
+        List<AprilTagDetection> detections = aprilTag.getDetections();
+        if (isRed) {
+            for (AprilTagDetection det : detections) {
+                if (det.id == 24) {
+                    return det.ftcPose.range;
+                }
+            }
+        } else {
+            for (AprilTagDetection det : detections) {
+                if (det.id == 20) {
+                    return det.ftcPose.range;
+                }
+            }
+        }
+        return -1.0;
     }
 }
