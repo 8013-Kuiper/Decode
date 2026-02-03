dc6d7da018fa4999316c7a217926e3d8a2456eba
Rowan Shenoy <rowans@hotmail.com>
2026-02-03 15:24:17 -0500

    Update PowerFunctionTool.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Unused/PowerFunctionTool.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Unused/PowerFunctionTool.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Unused;
 
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
