71314988e3c2f761e4f31864714f8ac3fbbafdb2
Rowan Shenoy <rowans@hotmail.com>
2026-02-03 18:05:43 -0500

    Create GoalTrack.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoalTrack.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@TeleOp
+public class GoalTrack extends OpMode {
+    final double TicksPerDeg = 4.4546;
+
+    Pose currentPose;
+    Follower follower;
+    boolean detectBlue=true;
+
+    Pose blueGoal = new Pose(6,133);
+    Pose redGoal = new Pose(144-6, 133 );
+
+    DcMotor turretRotation;
+    double angle;
+
+
+    public void init () {
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(currentPose);
+        follower.update();
+
+        turretRotation = hardwareMap.get(DcMotor.class, "turret");
+
+        turretRotation.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        currentPose = new Pose(72,72, Math.toRadians(0));
+    }
+
+    @Override
+    public void start() {
+        if (detectBlue) {
+            angle = Math.atan((currentPose.getX() - blueGoal.getX())/(currentPose.getY()) - blueGoal.getY());
+        }
+
+        follower.startTeleOpDrive();
+
+        turretRotation.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        turretRotation.setTargetPosition((int) ((angle - follower.getPose().getHeading())/TicksPerDeg));
+    }
+
+    @Override
+    public void loop() {
+        if (detectBlue) {
+            angle = Math.atan((currentPose.getX() - blueGoal.getX())/(currentPose.getY()) - blueGoal.getY());
+        }
+
+        turretRotation.setTargetPosition((int) ((angle - follower.getPose().getHeading())/TicksPerDeg));
+
+        follower.update();
+        currentPose = follower.getPose();
+        follower.setTeleOpDrive(
+                -gamepad1.left_stick_y,
+                gamepad1.left_stick_x,
+                gamepad1.right_stick_x);
+
+        turretRotation.setPower(0.25);
+    }
+}
