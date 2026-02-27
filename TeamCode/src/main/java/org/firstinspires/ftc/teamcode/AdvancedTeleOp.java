d7f295a7cc4dcdf68380a1d03a97820924f13800
Rowan Shenoy <rowans@hotmail.com>
2026-02-27 16:29:29 -0500

    disables opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AdvancedTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AdvancedTeleOp.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -16,6 +17,7 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import java.util.List;
 
 @TeleOp (name="**TeleOp With April Tags**")
+@Disabled
 public class AdvancedTeleOp extends OpMode {
     // Drive Motors
     DcMotor frontLeft, frontRight, backLeft, backRight;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
-@Disabled
+//@Disabled
 @TeleOp
 public class FinalTeleOp extends OpMode {
     //Turret constants
